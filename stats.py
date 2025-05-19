01376a0b0462b708d2fac7c1b8a85ddf07ba78e2
Nathanphillips-info <nathan.phillips1234@outlook.com>
2025-05-19 07:49:02 -0600

    corrected functions in stats.py

--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
- def get_num_words():
+from main import get_book_text
+def get_num_words():
     text = get_book_text("books/frankenstein.txt")
-    text.split()
     word_count = len(text.split()) 
-    return print(f"{word_count} words found in the document")
+    return word_count
