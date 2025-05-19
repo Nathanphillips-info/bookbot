61fc610606a5a6b23ada560958f401b361d36870
Nathanphillips-info <nathan.phillips1234@outlook.com>
2025-05-19 07:16:45 -0600

    new function for counting words commit.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    text.split()
+    word_count = len(text.split()) 
+    return print(f"{word_count} words found in document")
+
+
+main()
+
+
