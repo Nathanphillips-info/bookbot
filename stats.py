3f26154e37c7762142a5e51e200b7bbdb49312f1
Nathanphillips-info <nathan.phillips1234@outlook.com>
2025-05-22 07:34:09 -0600

    fixed count characters function

--- stats.py
+++ stats.py
@@ -9,7 +9,7 @@ def get_num_words():
     return (f"{word_count} words found in the document") 
 
 def count_characters(path_to_file):
-    text = get_book_text(path_to_file)
+    text = get_book_text("books/frankenstein.txt")
     text = text.lower() 
     word_dict = {}
     for i in text:
