8886a253d4d76d655bee0ee13366c1a4101b798b
Nathanphillips-info <nathan.phillips1234@outlook.com>
2025-05-22 07:19:28 -0600

    added path to file

--- stats.py
+++ stats.py
@@ -7,3 +7,19 @@ def get_num_words():
     text = get_book_text("books/frankenstein.txt")
     word_count = len(text.split()) 
     return (f"{word_count} words found in the document") 
+
+def count_characters(path_to_file):
+    text = get_book_text(path_to_file)
+    text = text.lower() 
+    word_dict = {}
+    for i in text:
+        if i in word_dict:
+            word_dict[i] += 1
+        else:
+            word_dict[i] = 1
+
+    return word_dict
+
+
+
+    
