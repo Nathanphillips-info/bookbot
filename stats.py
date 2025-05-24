29cfd17556535530b8552adb608231dcec397e29
Nathanphillips-info <nathan.phillips1234@outlook.com>
2025-05-24 16:51:24 -0600

    finished stats.py file

--- stats.py
+++ stats.py
@@ -9,7 +9,7 @@ def get_num_words():
     return (f"{word_count} words found in the document") 
 
 def count_characters(path_to_file):
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(path_to_file)
     text = text.lower() 
     word_dict = {}
     for i in text:
@@ -19,7 +19,19 @@ def count_characters(path_to_file):
             word_dict[i] = 1
 
     return word_dict
+word_dict = count_characters("books/frankenstein.txt")
+def create_list(word_dict): 
+    dict_list = []
+    for item, number in word_dict.items():
+        entry = {"char": item, "num": number} 
+        dict_list.append(entry)
+        
+    return dict_list 
 
+def sort_on(dict):
+    return dict["num"]
 
+dict_list = create_list(word_dict)
+dict_list.sort(reverse=True, key=sort_on)
+print(dict_list)
 
-    
