6f9541db71ad1267d0fbb018eacaaaf2b1d5c4e1
Nathanphillips-info <nathan.phillips1234@outlook.com>
2025-05-26 13:15:48 -0600

    almost finished main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from stats import get_num_words, count_characters, get_book_text
+from stats import get_num_words, count_characters, get_book_text, create_list, sort_on
 
 def main():
-    return get_num_words()
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = get_num_words()
+    letter_count = count_characters(book_text)
+    word_dict = count_characters(book_text)
+    dict_list = create_list(word_dict)
+    dict_list.sort(reverse=True, key=sort_on)
     
-
-print(main())
-
-book_text = get_book_text("books/frankenstein.txt")
-letter_count = count_characters(book_text)
-
-for char, count in letter_count.items():
-    print(f"'{char}': {count}")
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words.")
+    print("--------- Character Count -------")
+    for item in dict_list:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")   
+main()
